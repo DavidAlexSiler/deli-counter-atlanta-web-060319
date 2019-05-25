afdfbcfd4241bdad430b1ccc1cf37e5a7283ed31
Alex Siler <davidalexsiler@gmail.com>
2019-05-25 02:17:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,10 @@ def line(katz_deli)
     puts "The line is currently empty."
   end
   if katz_deli.length > 0
-    line_with_names = []
     num = 1
     katz_deli.each do |name|
-      line_with_names << "The line is currently: #{num}. #{name}"
+      "The line is currently: #{num}. #{name}"
       num += 1
     end
-    # line_with_names.each do |msg|
-    #   puts msg
-    # end
   end
 end
